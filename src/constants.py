a69ce5fea21de54394812b05d3250f936063f004
TarikHaci <tarikhaci@outlook.com>
2021-02-27 10:04:53 +0100

    Removed Repetative code to one method in the ProtocolHandler and added more constants

--- src/constants.py
+++ src/constants.py
@@ -2,5 +2,17 @@ from enum import Enum
 
 
 class Constants(Enum):
-    ENDPOINT = "https://whateverEndPoint"
+    ##API
+    API_URL = "https://whateverEndPoint"
+    ENDPOINT_AUTH_LOGIN = "/Authentication/Login"
+    ENDPOINT_DEVICE_HEARTBEAT = "/Device/Heartbeat"
+    ENDPOINT_DEVICE_CANPICKUP = "/Device/CanPickup"
+    ENDPOINT_DEVICE_PICKEDUPOBJECT = "/Device/PickedUpObject"
+    ENDPOINT_DEVICE_PUTBACKOBJECT = "/Device/PutBackObject"
+    ENDPOINT_DETERMINED_OBJECT = "/Device/DeterminedObject"
+    ENDPOINT_DEVICE_LOG = "/Device/Log"
+    ENDPOINT_DEVICE_SENSORDATA = "/Device/SensorData"
+
+    ##Brightness
     WHITE_VALUE: int = 35
+    BLACK_VALUE: int = 15
--- src/protocol.py
+++ src/protocol.py
@@ -6,53 +6,67 @@ from constants import Constants
 class Protocol:
     __token = None
 
+    def __check_response_status(self, dictResp):
+        #here error handler
+        #switch case (disctResp['statuscode'])
+        #case 200: Succes
+        #case 400: Return error to log (Terminal + API log)
+        return True
+
+    def __get_request(self, endpoint, headers = {}, bool_token = True):
+        if (bool_token and self.__token is not None):
+            headers['auth'] = {'auth': self.__token}
+        response = requests.get(Constants.API_URL + endpoint, headers=headers)
+        dictResp = json.loads(response.text)
+        # todo-> Check status code before returning (error handling) 200 good, but when it returns lik 400 then it is error.
+        return dictResp
+
+    def __post_request(self, endpoint, headers = {}, data = {}, bool_token = True):
+        ## Adding auth automatically in order to avoid repititve code, since token is always required by post except one case.
+        if (bool_token and self.__token is not None):
+            headers['auth'] = {'auth': self.__token}
+        response = requests.post(Constants.API_URL + endpoint, data=data, headers=headers)
+        dictResp = json.loads(response.text)
+        # todo-> Check status code before returning (error handling) 200 good, but when it returns lik 400 then it is error.
+        return dictResp
+
     def login(self):
         headers = {'Content-Type': 'application/json'}
         data = {'User': 'Username', 'Password': 'Password'}
-        response = requests.post(Constants.ENDPOINT + "/Authentication/Login", data=data, headers=headers)
-        dictResp = json.loads(response.text)
+        dictResp = self.__post_request(Constants.ENDPOINT_AUTH_LOGIN, headers, data, False)
         self.__token = str(dictResp['Token'])
 
     def heartbeat(self):
-        if self.__token is not None:
-            headers = {'auth': self.__token}
-            requests.get(Constants.ENDPOINT + "/Device/Heartbeat", headers=headers)
+        dictResp = self.__get_request(Constants.ENDPOINT_DEVICE_HEARTBEAT)
+        ## check if succes code 200 is return or not. Or maybe checking inside __post_request (errror handler).
 
     def can_pickup(self):
-        if self.__token is not None:
-            headers = {'auth': self.__token}
-            response = requests.get(Constants.ENDPOINT + "/Device/CanPickup", headers=headers)
-            dictResp = json.loads(response.text)
-            return dictResp['Resp']
+        dictResp = self.__get_request(Constants.ENDPOINT_DEVICE_CANPICKUP)
+        ## check if succes code 200 is return or not. Or maybe checking inside __post_request (errror handler).
+        return dictResp['Resp']
 
     def picked_up_object(self):
-        if self.__token is not None:
-            headers = {'auth': self.__token}
-            response = requests.post(Constants.ENDPOINT + "/Device/PickedUpObject", headers=headers)
-            dictResp = json.loads(response.text)
-            return dictResp['Resp']
+        dictResp = self.__post_request(Constants.ENDPOINT_DEVICE_PICKEDUPOBJECT)
+        return dictResp['Resp']
 
     def put_back_object(self):
-        if self.__token is not None:
-            headers = {'auth': self.__token}
-            response = requests.post(Constants.ENDPOINT + "/Device/PutBackObject", headers=headers)
-            dictResp = json.loads(response.text)
-            return dictResp['Resp']
+        dictResp = self.__post_request(Constants.ENDPOINT_DEVICE_PUTBACKOBJECT)
+        return dictResp['Resp']
 
     def determined_object(self, color: int):
-        if self.__token is not None:
-            headers = {'Content-Type': 'application/json', 'auth': self.__token}
-            data = {'Color': color}
-            requests.post(Constants.ENDPOINT + "/Device/DeterminedObject", data=data, headers=headers)
+        headers = {'Content-Type': 'application/json'}
+        data = {'Color': color}
+        dictResp = self.__post_request(Constants.ENDPOINT_DETERMINED_OBJECT, headers, data)
+        ## check if succes code 200 is return or not. Or maybe checking inside __post_request (errror handler).
 
     def log(self, tags: list, message: str):
-        if self.__token is not None:
-            headers = {'Content-Type': 'application/json', 'auth': self.__token}
-            data = {'Tags': tags, 'Message': message}
-            requests.post(Constants.ENDPOINT + "/Device/Log", data=data, headers=headers)
+        headers = {'Content-Type': 'application/json'}
+        data = {'Tags': tags, 'Message': message}
+        dictResp = self.__post_request(Constants.ENDPOINT_DEVICE_LOG, headers, data)
+        ## check if succes code 200 is return or not. Or maybe checking inside __post_request (errror handler).
 
     def sensor_data(self, data: dict):
-        if self.__token is not None:
-            headers = {'Content-Type': 'application/json', 'auth': self.__token}
-            data = {'Data': data}
-            requests.post(Constants.ENDPOINT + "/Device/SensorData", data=data, headers=headers)
+        headers = {'Content-Type': 'application/json'}
+        data = {'Data': data}
+        dictResp = self.__post_request(Constants.ENDPOINT_DEVICE_SENSORDATA, headers, data)
+        ## check if succes code 200 is return or not. Or maybe checking inside __post_request (errror handler).
