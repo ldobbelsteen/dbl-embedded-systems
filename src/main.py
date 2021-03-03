16df723558b1de5d9631872e06d124a933bdd19a
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2021-03-03 11:59:15 +0000

    Add basic reference code for arm prototype

--- src/main.py
+++ src/main.py
@@ -8,54 +8,42 @@ import switch
 
 def main():
 
-    # Test first motor
     motor1 = motor.Motor(16, 20, 21)
-    motor1.change_w_dur(True, 70, 1)
-    motor1.change_w_dur(False, 80, 1)
-
-    # Test second motor
     motor2 = motor.Motor(9, 11, 10)
-    motor2.change_w_dur(True, 90, 1)
-    motor2.change_w_dur(False, 60, 1)
-
-    # Test running both motors simultaneously
-    motor1.change(False, 80)
-    motor2.change(True, 100)
-    time.sleep(1)
-    motor1.stop()
-    motor2.stop()
-
-    # Test belt
-    belt1 = belt.Belt(motor1)
-    belt1.forward(100)
-    time.sleep(1)
+    switch1 = switch.Switch(13)
+    switch2 = switch.Switch(19)
+    led1 = led.Led(26)
+    photo1 = phototransistor.Phototransistor(14, 15, 18, 23)
 
     # Test sorting belt
-    belt2 = belt.SortingBelt(motor2)
-    belt2.white()
+    belt1 = belt.SortingBelt(motor1)
+    belt1.white()
     time.sleep(1)
-    belt2.black()
+    belt1.black()
     time.sleep(1)
+    belt1.stop()
 
-    # Test switch
-    switch1 = switch.Switch(13)
-    for i in range(20):
-        print(switch1.pressed())
-        time.sleep(0.05)
-
-    # Test controlling light
-    led1 = led.Led(26)
-    led1.on()
-    time.sleep(1)
-    led1.off()
+    # Test arm
+    motor2.change(True, 80)
+    while True:
+        if switch1.pressed():
+            motor2.change(False, 80)
+            while True:
+                if switch2.pressed():
+                    motor2.stop()
+                    break
+                time.sleep(0.01)
+            break
+        time.sleep(0.01)
 
     # Test phototransistor
-    photo = phototransistor.Phototransistor(14, 15, 18, 23)
-    while True:
-        reading = photo.get_reading(0)
-        color = photo.get_color(reading)
-        print(reading, color)
-        time.sleep(0.3)
+    led1.on()
+    time.sleep(0.5)
+    reading = photo1.get_reading(0)
+    color = photo1.get_color(reading)
+    print(reading, color)
+    time.sleep(0.5)
+    led1.off()
 
 if __name__ == '__main__':
     try:
