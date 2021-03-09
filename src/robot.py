6faffa91c4f6e102d1ce75d82377b02581f2b79e
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2021-03-09 12:55:53 +0100

    Fix Robot class

--- src/robot.py
+++ src/robot.py
@@ -11,14 +11,19 @@ class Robot:
 
     def __init__(self, mtr: motor.Motor, s_arrival: switch.Switch, s_start: switch.Switch):
         self.__motor = mtr
+        self.__switch_arrival = s_arrival
+        self.__switch_start = s_start
+        GPIO.add_event_detect(self.__switch_arrival.get_pin(), GPIO.RISING, callback=self.arm_arrived)
+        GPIO.add_event_detect(self.__switch_start.get_pin(), GPIO.RISING, callback=self.arm_back)
+        if not self.__switch_start.pressed():
+            self.arm_move_back()
+            time.sleep(2)
 
     def arm_push_off(self):
         self.__motor.change(True, Constants.ROBOT_MOTOR_POWER.value)
-        GPIO.add_event_detect(self.__switch_arrival.get_pin(), GPIO.RISING, callback=self.arm_arrived())
-
+        
     def arm_move_back(self):
         self.__motor.change(False, Constants.ROBOT_MOTOR_POWER.value)
-        GPIO.add_event_detect(self.__switch_start.get_pin(), GPIO.RISING, callback=self.arm_back())
 
     def arm_arrived(self):
         self.__motor.stop()
