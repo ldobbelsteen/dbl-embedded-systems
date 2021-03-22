4f34bcd645f6d0d42e8068f241c1f5d84f297ca9
TarikHaci <tarikhaci@outlook.com>
2021-03-22 12:41:08 +0100

    Version 3 black percetnage 100?

--- src/object_detection.py
+++ src/object_detection.py
@@ -140,7 +140,7 @@ class ObjectDetection:
 def main():
 #   #oo: ObjectDetection = ObjectDetection("object_detection/modules/default/model.tflite", "object_detection/modules/default/labels.txt", 640, 480, 0.2)
 #   #oo: ObjectDetection = ObjectDetection("object_detection/modules/teachablemachine/quantized/model.tflite", "object_detection/modules/teachablemachine/quantized/labels.txt", 480, 480, 0.4)
-  oo: ObjectDetection = ObjectDetection("object_detection/v2/model.tflite", "object_detection/v2/labels.txt", 480, 480, 0.95)
+  oo: ObjectDetection = ObjectDetection("object_detection/v3/model.tflite", "object_detection/v3/labels.txt", 480, 480, 0.95)
 #   print("initialized")
   while True:
     print(oo.get_detected_objects())
